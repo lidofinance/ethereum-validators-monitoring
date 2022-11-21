94a86012a7e0af702a7ac940f97ad04fc0880ea6
Vladimir Gorkavenko <32727352+vgorkavenko@users.noreply.github.com>
2022-11-21 11:48:10 +0500

    chore: remove extra workflows (#64)

--- src/common/eth-providers/consensus-provider/consensus-provider.module.ts
+++ src/common/eth-providers/consensus-provider/consensus-provider.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 
 import { BlockCacheModule } from './block-cache';
 import { ConsensusProviderService } from './consensus-provider.service';
-import { BlockCacheService } from './block-cache.service';
 
 @Module({
   imports: [BlockCacheModule],
