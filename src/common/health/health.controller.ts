ca6cc14c07eb905839e6cab4c442620c1f051aca
vgorkavenko <vladimir.g@lido.fi>
2022-11-24 13:44:02 +0500

    fix: health check

--- src/common/health/health.controller.ts
+++ src/common/health/health.controller.ts
@@ -1,3 +1,5 @@
+import * as v8 from 'v8';
+
 import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService, MemoryHealthIndicator } from '@nestjs/terminus';
 
@@ -5,11 +7,14 @@ import { HEALTH_URL } from './health.constants';
 
 @Controller(HEALTH_URL)
 export class HealthController {
-  constructor(private health: HealthCheckService, private memory: MemoryHealthIndicator) {}
+  private readonly maxHeapSize: number;
+  constructor(private health: HealthCheckService, private memory: MemoryHealthIndicator) {
+    this.maxHeapSize = v8.getHeapStatistics().heap_size_limit;
+  }
 
   @Get()
   @HealthCheck()
   check() {
-    return this.health.check([async () => this.memory.checkHeap('memoryHeap', 1024 * 1024 * 1024)]);
+    return this.health.check([async () => this.memory.checkHeap('memoryHeap', this.maxHeapSize)]);
   }
 }
