682520f63ba8148729ff66c0f36ff6680b354df0
vgorkavenko <vladimir.g@lido.fi>
2022-12-19 15:38:41 +0500

    wip: attestation rewards summ accuracy

--- src/duty/attestation/attestation.constants.ts
+++ src/duty/attestation/attestation.constants.ts
@@ -23,12 +23,17 @@ export const timelySource = (att_inc_delay: number, att_valid_source: boolean): 
   if (att_valid_source && att_inc_delay <= 5) return TIMELY_SOURCE_WEIGHT;
   else return 0;
 };
-export const timelyTarget = (att_inc_delay: number, att_valid_target: boolean): number => {
-  if (att_valid_target && att_inc_delay <= 32) return TIMELY_TARGET_WEIGHT;
+export const timelyTarget = (att_inc_delay: number, att_valid_source: boolean, att_valid_target: boolean): number => {
+  if (att_valid_source && att_valid_target && att_inc_delay <= 32) return TIMELY_TARGET_WEIGHT;
   else return 0;
 };
-export const timelyHead = (att_inc_delay: number, att_valid_head: boolean): number => {
-  if (att_valid_head && att_inc_delay == 1) return TIMELY_HEAD_WEIGHT;
+export const timelyHead = (
+  att_inc_delay: number,
+  att_valid_source: boolean,
+  att_valid_target: boolean,
+  att_valid_head: boolean,
+): number => {
+  if (att_valid_source && att_valid_target && att_valid_head && att_inc_delay == 1) return TIMELY_HEAD_WEIGHT;
   else return 0;
 };
 export const attestationRewards = (
@@ -39,8 +44,8 @@ export const attestationRewards = (
 ) => {
   return {
     source: timelySource(att_inc_delay, att_valid_source) / WEIGHT_DENOMINATOR,
-    target: timelyTarget(att_inc_delay, att_valid_target) / WEIGHT_DENOMINATOR,
-    head: timelyHead(att_inc_delay, att_valid_head) / WEIGHT_DENOMINATOR,
+    target: timelyTarget(att_inc_delay, att_valid_source, att_valid_target) / WEIGHT_DENOMINATOR,
+    head: timelyHead(att_inc_delay, att_valid_source, att_valid_target, att_valid_head) / WEIGHT_DENOMINATOR,
   };
 };
 
@@ -49,12 +54,12 @@ const untimelySource = (att_inc_delay: number, att_valid_source: boolean): numbe
   if (!timelySource(att_inc_delay, att_valid_source)) return TIMELY_SOURCE_WEIGHT;
   else return 0;
 };
-const untimelyTarget = (att_inc_delay: number, att_valid_target: boolean): number => {
-  if (!timelyTarget(att_inc_delay, att_valid_target)) return TIMELY_TARGET_WEIGHT;
+const untimelyTarget = (att_inc_delay: number, att_valid_source: boolean, att_valid_target: boolean): number => {
+  if (!timelyTarget(att_inc_delay, att_valid_source, att_valid_target)) return TIMELY_TARGET_WEIGHT;
   else return 0;
 };
-const untimelyHead = (att_inc_delay: number, att_valid_head: boolean): number => {
-  if (!timelyHead(att_inc_delay, att_valid_head)) return 0;
+const untimelyHead = (att_inc_delay: number, att_valid_source: boolean, att_valid_target: boolean, att_valid_head: boolean): number => {
+  if (!timelyHead(att_inc_delay, att_valid_source, att_valid_target, att_valid_head)) return 0;
   else return 0;
 };
 export const attestationPenalties = (
@@ -65,7 +70,7 @@ export const attestationPenalties = (
 ) => {
   return {
     source: untimelySource(att_inc_delay, att_valid_source) / WEIGHT_DENOMINATOR,
-    target: untimelyTarget(att_inc_delay, att_valid_target) / WEIGHT_DENOMINATOR,
-    head: untimelyHead(att_inc_delay, att_valid_head) / WEIGHT_DENOMINATOR,
+    target: untimelyTarget(att_inc_delay, att_valid_source, att_valid_target) / WEIGHT_DENOMINATOR,
+    head: untimelyHead(att_inc_delay, att_valid_source, att_valid_target, att_valid_head) / WEIGHT_DENOMINATOR,
   };
 };
