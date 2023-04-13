006ec380f56446e1c7e1f0770078a89e65bec27b
Vladimir Gorkavenko <32727352+vgorkavenko@users.noreply.github.com>
2023-04-13 23:01:57 +0500

    fix: critical slashing alert (#159)
    * fix: critical slashing alert
    
    * fix: remove useless condition

--- src/common/alertmanager/alerts/CriticalNegativeDelta.ts
+++ src/common/alertmanager/alerts/CriticalNegativeDelta.ts
@@ -52,7 +52,7 @@ export class CriticalNegativeDelta extends Alert {
   alertBody(ruleResult: AlertRuleResult): AlertRequestBody {
     return {
       startsAt: new Date(this.sendTimestamp).toISOString(),
-      endsAt: new Date(new Date(this.sendTimestamp).setMinutes(new Date(this.sendTimestamp).getMinutes() + 1)).toISOString(),
+      endsAt: new Date(new Date(this.sendTimestamp).setMinutes(new Date(this.sendTimestamp).getMinutes() + 2)).toISOString(),
       labels: { alertname: this.alertname, severity: 'critical', ...this.config.get('CRITICAL_ALERTS_ALERTMANAGER_LABELS') },
       annotations: {
         summary: `${Object.values(ruleResult).length} Node Operators with CRITICAL count of validators with negative delta`,
